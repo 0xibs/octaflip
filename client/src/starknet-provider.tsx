f09565c25044dfcb7f2a8ecf6ab2d2c1e57436ae
Ibrahim Suleiman <suleimanonnet@gmail.com>
2025-02-21 14:40:29 +0100

    feat: integrate cartridge

--- client/src/starknet-provider.tsx
+++ client/src/starknet-provider.tsx
@@ -1,33 +1,95 @@
-import type { PropsWithChildren } from "react";
-import { mainnet } from "@starknet-react/chains";
-import { jsonRpcProvider, StarknetConfig, voyager } from "@starknet-react/core";
-import { dojoConfig } from "../dojoConfig";
+import { sepolia, mainnet, Chain } from "@starknet-react/chains";
+import { constants } from 'starknet'
 import {
-  predeployedAccounts,
-  PredeployedAccountsConnector,
-} from "@dojoengine/predeployed-connector";
+    StarknetConfig,
+    jsonRpcProvider,
+    starkscan,
+} from "@starknet-react/core";
+import ControllerConnector from "@cartridge/connector/controller";
+import { SessionPolicies } from "@cartridge/controller";
 
-let pa: PredeployedAccountsConnector[] = [];
-predeployedAccounts({
-  rpc: dojoConfig.rpcUrl as string,
-  id: "katana",
-  name: "Katana",
-}).then((p) => (pa = p));
+// Define your contract addresses
+const ETH_TOKEN_ADDRESS =
+    '0x049d36570d4e46f48e99674bd3fcc84644ddd6b96f7c741b1562b82f9e004dc7'
 
-export default function StarknetProvider({ children }: PropsWithChildren) {
-  const provider = jsonRpcProvider({
-    rpc: () => ({ nodeUrl: dojoConfig.rpcUrl as string }),
-  });
+// Define session policies
+const policies: SessionPolicies = {
+    contracts: {
+        [ETH_TOKEN_ADDRESS]: {
+            methods: [
+                {
+                    name: "approve",
+                    entrypoint: "approve",
+                    description: "Approve spending of tokens",
+                },
+                { name: "transfer", entrypoint: "transfer" },
+            ],
+        },
+    },
+}
 
-  return (
-    <StarknetConfig
-      chains={[mainnet]}
-      provider={provider}
-      connectors={pa}
-      explorer={voyager}
-      autoConnect
-    >
-      {children}
-    </StarknetConfig>
-  );
+// Initialize the connector
+const connector = new ControllerConnector({
+    policies,
+    chains: [
+        {
+            rpcUrl:
+
+                "https://api.cartridge.gg/x/starknet/sepolia",
+        },
+        {
+            rpcUrl:
+
+                "https://api.cartridge.gg/x/starknet/mainnet",
+        },
+    ],
+    defaultChainId: constants.StarknetChainId.SN_SEPOLIA,
+    // url:
+    //     process.env.NEXT_PUBLIC_KEYCHAIN_DEPLOYMENT_URL ??
+    //     process.env.NEXT_PUBLIC_KEYCHAIN_FRAME_URL,
+    // profileUrl:
+    //     process.env.NEXT_PUBLIC_PROFILE_DEPLOYMENT_URL ??
+    //     process.env.NEXT_PUBLIC_PROFILE_FRAME_URL,
+    // slot: "profile-example",
+    slot: "",
+    preset: "octoflip",
+    // namespace: "dopewars",
+    // slot: "eternum-prod",
+    // preset: "eternum",
+    // namespace: "s0_eternum",
+    // slot: "darkshuffle-mainnet",
+    // preset: "dark-shuffle",
+    // namespace: "darkshuffle_s0",
+    tokens: {
+        erc20: [
+            "0x0124aeb495b947201f5fac96fd1138e326ad86195b98df6dec9009158a533b49",
+        ],
+    },
+});
+
+// Configure RPC provider
+const provider = jsonRpcProvider({
+    rpc: (chain: Chain) => {
+        switch (chain) {
+            case mainnet:
+                return { nodeUrl: 'https://api.cartridge.gg/x/starknet/mainnet' }
+            case sepolia:
+            default:
+                return { nodeUrl: 'https://api.cartridge.gg/x/starknet/sepolia' }
+        }
+    },
+})
+
+export function StarknetProvider({ children }: { children: React.ReactNode }) {
+    return (
+        <StarknetConfig
+            autoConnect
+            chains={[mainnet, sepolia]}
+            provider={provider}
+            connectors={[connector]}
+            explorer={starkscan}
+        >
+            {children}
+        </StarknetConfig>
+    )
 }
